66f83a473b8b6dc32bd1bb4e8c181632ccc11e7b
Irresistible-Ishan <1ishanmanisingh@gmail.com>
2026-06-07 18:15:51 +0530

    testing a basic gateway to wallhaven api

--- nodejs-refresh/app.js
+++ nodejs-refresh/app.js
@@ -1,1 +1,13 @@
-npm pkg set type="module"
+import express from 'express';
+
+const app = express();
+const PORT = 6969;
+
+app.get('/image/random', (req, res) => {
+    console.log(`[SERVER] Redirecting client to: https://picsum.photos/id/1024/800/600`);
+    res.redirect('https://picsum.photos/id/1024/800/600');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/image/random`);
+});
