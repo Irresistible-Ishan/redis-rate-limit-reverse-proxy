81822dc3670a019c6a19a8549e5828d2cf5ccf8f
Irresistible-Ishan <1ishanmanisingh@gmail.com>
2026-06-18 19:01:36 +0530

    async

--- target-api/app.js
+++ target-api/app.js
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express();
 const PORT = 6969;
 
-app.get('/', (req, res) => {
-    console.log(`[SERVER] Redirecting client to: https://picsum.photos/id/1024/800/600`);
-    res.redirect('https://wallhaven.cc/api/v1/search?sorting=date_added&order=desc&categories=010&purity=100&page=1&apikey=jvlElNDcAQb4IbAL2S6JvkGDtMbgGRD2');
+app.get('/', async (req, res) => {
+    const response = await fetch('https://wallhaven.cc/api/v1/search?sorting=toplist&order=desc&categories=100&purity=100&page=1&apikey=jvlElNDcAQb4IbAL2S6JvkGDtMbgGRD2');
+    const jsonData = await response.json();
+    const imagePath = jsonData.data[0].path;
+    res.redirect(imagePath);
 });
 
 app.listen(PORT, () => {
